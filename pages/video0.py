73cab76f6800a87daf4d032e1fba93486857f530
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-13 17:45:01 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -5,13 +5,13 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.write('원본 영상')
-    video_file = open('https://youtu.be/Dan-UpqtzL4', 'rb')
+    video_file = open('https://youtu.be/Dan-UpqtzL4', format="video/mp4")
     video_bytes = video_file.read()
 
     st.video(video_bytes)
 with col2:
     st.write('분석 영상')
-    hiera_video_file = open('https://youtu.be/Dan-UpqtzL4', 'rb')
+    hiera_video_file = open('https://youtu.be/Dan-UpqtzL4', format="video/mp4")
     hiera_video_bytes = hiera_video_file.read()
 
     st.video(hiera_video_bytes)
@@ -20,4 +20,4 @@ with col3:
 
 
 st.write('분석 과정')
-st.image('/home/vision/Capstone/video/hiera_image/ge_out21_hiera.png')
+#st.image('/home/vision/Capstone/video/hiera_image/ge_out21_hiera.png')
