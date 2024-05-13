443fcda9696cbdc1dcb80d0725e9d0fc9af859f9
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-13 17:53:05 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -5,16 +5,11 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.write('원본 영상')
-    video_file = open('https://youtu.be/Dan-UpqtzL4?si=VfdQ9MRT4e7F2SH9', format="video/mp4")
-    video_bytes = video_file.read()
+    st.video('https://youtu.be/Dan-UpqtzL4?si=VfdQ9MRT4e7F2SH9', format="video/mp4")
 
-    st.video(video_bytes)
 with col2:
     st.write('분석 영상')
-    hiera_video_file = open('https://youtu.be/Dan-UpqtzL4?si=VfdQ9MRT4e7F2SH9', format="video/mp4")
-    hiera_video_bytes = hiera_video_file.read()
-
-    st.video(hiera_video_bytes)
+    st.video('https://youtu.be/Dan-UpqtzL4?si=VfdQ9MRT4e7F2SH9', format="video/mp4")
 with col3:
     st.error('OUT')
 
