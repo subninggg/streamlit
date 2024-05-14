f97fa4a957009f5a014825474e7e6235221ba0f6
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-14 10:59:04 +0900

    Update video0_sttran.py

--- pages/video0_sttran.py
+++ pages/video0_sttran.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.write('분석 영상')
 st.write('분석 영상')
-    st.video('https://file.notion.so/f/f/2abd79a2-2684-43c4-88e8-dff82ccba814/0296b0d3-38a0-4cd1-af82-a17c0db68778/ge_out21_hiera.mp4?id=e0cf9e81-396f-4bab-b838-0265b3425139&table=block&spaceId=2abd79a2-2684-43c4-88e8-dff82ccba814&expirationTimestamp=1715817600000&signature=K2Bvg6K1X29rN1jBpfcXHPSPHVkIJ-KRJapwG92OEdw&downloadName=ge_out21_hiera.mp4', format="video/mp4")
+st.video('https://file.notion.so/f/f/2abd79a2-2684-43c4-88e8-dff82ccba814/0296b0d3-38a0-4cd1-af82-a17c0db68778/ge_out21_hiera.mp4?id=e0cf9e81-396f-4bab-b838-0265b3425139&table=block&spaceId=2abd79a2-2684-43c4-88e8-dff82ccba814&expirationTimestamp=1715817600000&signature=K2Bvg6K1X29rN1jBpfcXHPSPHVkIJ-KRJapwG92OEdw&downloadName=ge_out21_hiera.mp4', format="video/mp4")
