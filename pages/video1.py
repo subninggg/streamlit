70663a1de4c95abc1d88bb531a7ec55de9190ff8
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 18:07:19 +0900

    Update video1.py

--- pages/video1.py
+++ pages/video1.py
@@ -5,14 +5,14 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.write('원본 영상')
-    st.video('https://file.notion.so/f/f/28b7e521-0e53-4f97-b19b-3c7008fb8ca0/3f81b632-d650-497e-a920-815c6369a2de/ge_safe2.mp4?id=f40ae133-b4fb-4ad5-88a3-14c673dffd0c&table=block&spaceId=28b7e521-0e53-4f97-b19b-3c7008fb8ca0&expirationTimestamp=1715688000000&signature=N3g8qZnk3FXJL6GO5QX4cOnW8FRRaFBrn6rE-ny9yrs&downloadName=ge_safe2.mp4', format="video/mp4")
+    st.image('https://github.com/subninggg/streamlit/blob/main/video_gif/ge_safe2.gif?raw=true')
 
 with col2:
     st.write('분석 영상')
-    st.video('https://youtu.be/Dan-UpqtzL4?si=VfdQ9MRT4e7F2SH9', format="video/mp4")
+    st.image('https://github.com/subninggg/streamlit/blob/main/hiera_gif/ge_safe2_hiera.gif?raw=true')
 with col3:
     st.error('OUT')
-    st.page_link("pages/video0_sttran.py", label="video0_sttran")
+    st.page_link("pages/video1_sttran.py", label="판정 분석")
 
 st.write('분석 과정')
 st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image/ge_safe2_hiera.png')
