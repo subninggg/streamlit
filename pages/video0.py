86aa3ffd0d800f5b631388176bde39b91325d1ac
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 18:22:53 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.write('원본 영상')
@@ -10,9 +10,9 @@ with col1:
 with col2:
     st.write('분석 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/hiera_gif/ge_out21_hiera.gif?raw=true')
-with col3:
-    st.error('OUT')
-    st.page_link("pages/video0_sttran.py", label="판정 분석")
+
+st.error('OUT')
+st.page_link("pages/video0_sttran.py", label="판정 분석")
 
 st.write('분석 과정')
 st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image/ge_out21_hiera.png')
