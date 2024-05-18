b000eaeb3ec71f382a7c9f2f4166ba48e3b39790
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 19:43:12 +0900

    Update video3.py

--- pages/video3.py
+++ pages/video3.py
@@ -30,10 +30,19 @@ with col2:
     st.subheader('분석 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/hiera_gif/ge_safe24_hiera.gif?raw=true')
 
-st.success('SAFE')
-
 st.subheader('')
 st.subheader('분석 과정')
 st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image/ge_safe24_hiera.png')
+
+progress_text = "Please wait..."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
+st.success('SAFE')
 
 st.page_link("pages/video3_sttran.py", label="판정 분석")
