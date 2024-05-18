3eef142e173ca21e592ce7ac823dd036de97871c
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 18:38:22 +0900

    Update video7.py

--- pages/video7.py
+++ pages/video7.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import time
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.write('원본 영상')
+    st.subheader('원본 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/video_gif/sb_out5.gif?raw=true')
 
 with col2:
-    st.write('분석 영상')
+    st.subheader('분석 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/hiera_gif/sb_out5_hiera.gif?raw=true')
-with col3:
-    st.error('OUT')
-    st.page_link("pages/video5_sttran.py", label="판정 분석")
 
-st.write('분석 과정')
+st.error('OUT')
+
+st.subheader('')
+st.subheader('분석 과정')
 st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image/sb_out5_hiera.png')
+
+st.page_link("pages/video7_sttran.py", label="판정 분석")
