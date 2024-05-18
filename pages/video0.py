e15d7554ace8c468e7082f239d295fd2e6e12c79
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 19:38:17 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -37,7 +37,7 @@ st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image
 progress_text = "Please wait..."
 my_bar = st.progress(0, text=progress_text)
 
-for percent_complete in range(500):
+for percent_complete in range(100):
     time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
 time.sleep(1)
