084630607a57e800baaede663c0d8e0fb0aa02ac
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-18 18:32:37 +0900

    Update video2.py

--- pages/video2.py
+++ pages/video2.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import time
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.write('원본 영상')
+    st.subheader('원본 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/video_gif/ge_safe20.gif?raw=true')
 
 with col2:
-    st.write('분석 영상')
+    st.subheader('분석 영상')
     st.image('https://github.com/subninggg/streamlit/blob/main/hiera_gif/ge_safe20_hiera.gif?raw=true')
-with col3:
-    st.error('OUT')
-    st.page_link("pages/video2_sttran.py", label="판정 분석")
 
-st.write('분석 과정')
+st.error('OUT')
+
+st.subheader('')
+st.subheader('분석 과정')
 st.image('https://raw.githubusercontent.com/subninggg/streamlit/main/hiera_image/ge_safe20_hiera.png')
+
+st.page_link("pages/video2_sttran.py", label="판정 분석")
