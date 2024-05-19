7e843fb020b86d91b57a43c8f27c08debe691ba9
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-19 13:25:08 +0900

    Update home.py

--- home.py
+++ home.py
@@ -4,11 +4,5 @@ import datetime
 
 st.title('LSK 2024')
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.page_link("pages/경기기록.py", label="경기기록")
-with col2:
-    st.page_link("pages/태그 판정.py", label="태그 판정")
-
+st.page_link("pages/경기기록.py", label="경기기록")
 st.page_link("https://www.kborc.com/main.do", label="KBO 비디오판독센터")
