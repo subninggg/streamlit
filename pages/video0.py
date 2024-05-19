b4b3897cec08f7de07362f906de343047b0d0124
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-19 12:01:58 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -37,7 +37,7 @@ for percent_complete in range(300):
     time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
 time.sleep(1)
-my_bar.empty()
+#my_bar.empty()
 
 st.error('OUT')
 
