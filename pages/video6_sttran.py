e065cb403413ba016e20b7f891ade112bdf8dbd1
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-19 15:36:03 +0900

    Update video6_sttran.py

--- pages/video6_sttran.py
+++ pages/video6_sttran.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.subheader('분석 영상')
 st.image('https://github.com/subninggg/streamlit/blob/main/sttran_gif/sb_out4_sttran.gif?raw=true')
+st.page_link("pages/video6.py", label="태그 영상")
