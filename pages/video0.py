295f90236dea9fa6d4341ffa17562d1ee080171c
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-19 12:03:21 +0900

    Update video0.py

--- pages/video0.py
+++ pages/video0.py
@@ -33,7 +33,7 @@ with col2:
 progress_text = "Please wait..."
 my_bar = st.progress(0, text=progress_text)
 
-for percent_complete in range(300):
+for percent_complete in range(100):
     time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
 time.sleep(1)
