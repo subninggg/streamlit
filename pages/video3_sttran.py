a58dffd9242c8351805fc9cb1d998e2d5e1eb91d
subninggg <104334903+subninggg@users.noreply.github.com>
2024-05-21 11:09:21 +0900

    Update video3_sttran.py

--- pages/video3_sttran.py
+++ pages/video3_sttran.py
@@ -21,4 +21,4 @@ with col3:
 
 st.subheader('분석 영상')
 st.image('https://github.com/subninggg/streamlit/blob/main/sttran_gif/ge_safe24_sttran.gif?raw=true')
-st.page_link("pages/video3.py", label="태그 영상")
+st.link_button("뒤로 가기", "https://homepy-wgogeawchtyohpnvkzfyzx.streamlit.app/video3")
